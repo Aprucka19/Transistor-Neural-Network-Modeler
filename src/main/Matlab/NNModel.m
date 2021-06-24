4f4416dd76edcf3d0ccb91b1d87fea56906e455f
Aprucka19 <awp@prucka.com>
2021-06-24 16:26:33 +0200

    NNModel.m optimization

--- src/main/Matlab/NNModel.m
+++ src/main/Matlab/NNModel.m
@@ -13,30 +13,22 @@ classdef NNModel
         function output = useModel(model, input)
             
             A = input;
-            
-            if(size(A,1) < 1001)
-                dblArray = javaArray('java.lang.Double',size(A,1),size(A,2));
-                JavaMatlabConvert = "edlab.eda.transistorModel.JavaMatlabConvert";
+            dblArray = javaArray('java.lang.Double',size(A,1),size(A,2));
+            JavaMatlabConvert = "edlab.eda.transistorModel.JavaMatlabConvert";
 
-                for m = 1:size(A,1)
-                    for n = 1:size(A,2)
-                        dblArray(m,n) = java.lang.Double(A(m,n));
-                    end
+            for m = 1:size(A,1)
+                for n = 1:size(A,2)
+                    dblArray(m,n) = java.lang.Double(A(m,n));
                 end
-
-                inputSArray = javaMethod("toJava",JavaMatlabConvert,dblArray);
-                
-                outputSArray = javaMethod("useModel",model.modelObject,inputSArray);
-                
-                outputDArray = javaMethod("toMatlab",JavaMatlabConvert,outputSArray);
-                output = double(outputDArray);
             end
-            if(size(A,1) > 1000)
-                
-                output = [model.useModel(A(1:1000,:)) ; model.useModel(A(1001:end,:))];
 
-            end
-    
+            inputSArray = javaMethod("toJava",JavaMatlabConvert,dblArray);
+
+            outputSArray = javaMethod("useModel",model.modelObject,inputSArray);
+
+            outputDArray = javaMethod("toMatlab",JavaMatlabConvert,outputSArray);
+            output = double(outputDArray);
+
        
         end
         
